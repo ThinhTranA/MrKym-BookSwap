fdf63c1c944679818d6d92429a012d9fcdf18ef7
Thinh Tran <thinhtran@outlook.com.au>
2019-09-21 22:17:29 +0800

    Basic grid layouts

---
+++ BookSwap/BookSwap/BookSwap/Models/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmHelpers;
+
+namespace BookSwap.Models
+{
+    public class Book : ObservableObject
+    {
+        private string _title;
+        private string _author;
+        private string _coverImage;
+        private string _accentColor;
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty<string>(ref _title, value);
+        }
+
+        public string Author
+        {
+            get => _author;
+            set => SetProperty<string>(ref _author, value);
+        }
+
+        public string CoverImage
+        {
+            get => _coverImage;
+            set => SetProperty<string>(ref _coverImage, value);
+        }
+
+        public string AccentColor
+        {
+            get => _accentColor;
+            set => SetProperty<string>(ref _accentColor, value);
+        }
+    }
+}
---
+++ BookSwap/BookSwap/BookSwap/ViewModels/BooksViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookSwap.Models;
+using MvvmHelpers;
+
+namespace BookSwap.ViewModels
+{
+    public class BooksViewModel : ObservableObject
+    {
+        public IList<Book> Books { get; set; }
+
+        public BooksViewModel()
+        {
+            Books = new ObservableRangeCollection<Book>()
+            {
+                new Book()
+                {
+                    Title = "Everything is Illuminated",
+                    Author = "Jonathan Safran Foer",
+                    AccentColor = "#0FF4C3",
+                    CoverImage = "book_illumated", 
+                },
+                new Book()
+                {
+                Title = "Ulysses",
+                Author = "James Joyce",
+                AccentColor = "#B76EFE",
+                CoverImage = "book_ulyssess",
+                },
+                new Book()
+                {
+                    Title = "Flowers for Algernon",
+                    Author = "Daniel Keyes",
+                    AccentColor = "#FF848b",
+                    CoverImage = "book_flowers",
+                },
+            };
+        }
+    }
+}
